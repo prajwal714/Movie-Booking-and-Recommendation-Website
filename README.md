3711ab63124db604a8c4f23ecd571ac2ce9eab6d
prajwal714 <prajwal714singh@gmail.com>
2020-07-22 22:37:48 +0530

    fixed security, enhanced Readme file

--- README.md
+++ README.md
@@ -1,45 +1,64 @@
 
+**Movie Booking and Recommendation Website**
 [![Movie Booking Website](http://i.imgur.com/sample/movie_booking.gif)]()
 
 # Movie-Booking-Website
-Movie Booking website using Nodejs, Express framework and MongoDB. EJS templates on frontend with HTML, Bootstrap and CSS.
+Movie Booking website using Nodejs, Express framework and MongoDB Atlas Cloud Database. EJS templates on frontend with HTML, Bootstrap and CSS. I did this project in my Sophmore Year as a part of my Winter project Competition organized by Developers Club of IIIT for which I won the first prize too.
 
 ## Access
 Their are two access-views of the website :
 - User View
 
-  Explore and Book Movies. View History, generate Invoice. 
+  Explore and Book Movies. View History, generate Invoice. Explore available seats and Book available ones.
   
 - Admin View
     This view is protected by authentication using passport.js
+    use the mentioned Username and password to access the admin view.
+   >**Username**: admin
+   >**Password**: admin
     
-    username: admin
-    password: admin
+    In this view their are multiple admin-side features :
     
-    In this view their are multiple admin features :
+   - Screening Movies: View, Edit and Delete currently screening movies or Add new movies for screenings.
     
-   >Screening Movies: View, Edit and Delete currently screening movies.
+   - Manange Screen: Assign Auditorium to movies, based on available slots. Their are 4 slots for now.
     
-   >Manange Screen: Assign Audi to movies, based on available slots.
-    
-   >Manage movies: Add new movies, update existing.
+   - Manage movies: Add new movies, update existing. Can also search and add latest movies, it utilizes    OMDB API to fetch and add new movies.
 
-## Demo
+## Prototype
+**Main Landing Page**
 [![Movie Booking Website](/sample/images/main_page.png)]()
+**Seat Booking Page**
 [![Movie Booking Website](/sample/images/booking_page.png)]()
+**Manage Movies Here**
 [![Movie Booking Website](/sample/images/movies_page.png)]()
+**Allot Auditorium**
 [![Movie Booking Website](/sample/images/admin_page.png)]()
+**View History and Generate Invoice**
+[![Movie Booking Website](/sample/images/invoice.png)]()
+
 
 
 ## Installation
 
-
 ### Clone 
-- Clone the repository to your local machine using git clone 
-- Change the Mongodb credentials in the code
-- `npm install`
+- Clone the repository to your local machine using git clone using `https://github.com/prajwal714/Movie-Booking-and-Recommendation-Website.git`
+
+### Setup
+- Run `npm install` to install all the packages and dependencies
+- Create a .env file, add the MONGODB_API path corresponding to your MongoDB atlas account in the file.
+   A .env.sample file is provided.
 
 ## About me
-I am Computer science Undergrad from IIIT Gwalior. Former intern at GoJek, passionate dev and tech enthusiast. Do connect with me.
+I am Computer Science Undergrad from IIIT Gwalior. Former intern at GoJek, passionate dev and tech enthusiast. I keep myself involved in building stuff and experimenting with new things. Do checkout my portfolio and connect with me on LinkedIn. 
+- **Portfolio** : <a href="https://prj-prajwal.netlify.app/" target="_blank">`prj-prajwal.netlify.app`</a>
+- **LinkedIn** <a href="https://www.linkedin.com/in/prajwal714/" target="_blank">`linkedin.com/in/prajwal714`</a>
 
-    
+
+
+## License
+
+[![License](http://img.shields.io/:license-mit-blue.svg?style=flat-square)](http://badges.mit-license.org)
+
+- **[MIT license](http://opensource.org/licenses/mit-license.php)**
+
--- app.js
+++ app.js
@@ -1,85 +1,85 @@
-const express=require("express"),
+const express = require("express"),
 	// MongoClient=require("mongodb").MongoClient,
-	app=express(),
-	mongoose=require("mongoose"),
-	flash=require("connect-flash"),
-	passport=require("passport"),
-    LocalStrategy=require("passport-local"),
-	passportLocalMongoose=require("passport-local-mongoose"),
-	
-	
+	app = express(),
+	mongoose = require("mongoose"),
+	flash = require("connect-flash"),
+	passport = require("passport"),
+	LocalStrategy = require("passport-local"),
+	helmet = require('helmet')
+dotenv = require('dotenv');
 
-	movies=require("./models/movies"),
-	audi=require("./models/audi"),
-	admin=require("./models/admin"),
-	user=require("./models/user"),
-	reservation=require("./models/reservation"),
-	screening=require("./models/screening"),
-	methodOverride=require("method-override"),
-	seed=require("./seed"),
+dotenv.config();
 
 
-	bodyParser=require("body-parser");
-	app.use(express.static("public"));
-	app.set("view engine","ejs");
-	app.use(bodyParser.urlencoded({extended:true}));
 
-	mongoose.connect("mongodb+srv://prajwal:prajwal71421@cluster0-mbqck.gcp.mongodb.net/mbook");
-	app.use(methodOverride("_method"));
-	app.use(flash());
+admin = require("./models/admin"),
 
-	seed();
 
-	
+	methodOverride = require("method-override"),
+	seed = require("./seed"),
 
-	var indexRoutes=require("./routes/index");
-	var moviesRoutes=require("./routes/movies");
-	var	screeningRoutes=require("./routes/screening");
-	var bookingRoutes=require("./routes/booking");
-	var authenticationRoutes=require("./routes/authentication");
-	var search=require("./routes/check");
-	var profile=require("./routes/profile");
 
-	app.use(require("express-session")(
-		{ 
-		  secret: "hello world",
-		  resave: false,
-		  saveUninitialized: false
+	bodyParser = require("body-parser");
+app.use(helmet());
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect(process.env.MONGODB_API, { useNewUrlParser: true, useUnifiedTopology: true });
 
-		}
-		));
-		app.use(passport.initialize());
-		app.use(passport.session());
-		passport.use(new LocalStrategy(admin.authenticate()));
-		passport.serializeUser(admin.serializeUser());
-		passport.deserializeUser(admin.deserializeUser());
+console.log("connected to DB");
+app.use(methodOverride("_method"));
+app.use(flash());
 
-		// passport.use(new LocalStrategy(user.authenticate()));
-		// passport.serializeUser(user.serializeUser());
-		// passport.deserializeUser(user.deserializeUser());
+seed();
+console.log("DB Seed complete");
 
-		 app.use(function(req,res,next)
-		{
-			console.log(req.user);
-		  //we pass the currentUser variable to every ejs tenplate which contains the info of current user
-		  res.locals.admin=req.user;
-		  res.locals.error=req.flash("error");
-		  res.locals.success=req.flash("success");
-		  next();
-		});
 
-		app.use(indexRoutes);	
-		app.use(moviesRoutes);
-		app.use(screeningRoutes);
-		app.use(bookingRoutes);
-		app.use(authenticationRoutes);
-		app.use(search);
-		app.use(profile);
 
-	
-	 
+var indexRoutes = require("./routes/index");
+var moviesRoutes = require("./routes/movies");
+var screeningRoutes = require("./routes/screening");
+var bookingRoutes = require("./routes/booking");
+var authenticationRoutes = require("./routes/authentication");
+var search = require("./routes/check");
+var profile = require("./routes/profile");
 
-   app.listen(process.env.PORT,process.env.IP,function()
+app.use(require("express-session")(
 	{
-		console.log("Server started at port 2000");
-	});
+		secret: "hello world",
+		resave: false,
+		saveUninitialized: false
+
+	}
+));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(admin.authenticate()));
+passport.serializeUser(admin.serializeUser());
+passport.deserializeUser(admin.deserializeUser());
+
+
+
+app.use(function (req, res, next) {
+	console.log(req.user);
+	//we pass the currentUser variable to every ejs tenplate which contains the info of current user
+	res.locals.admin = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+	next();
+});
+
+app.use(indexRoutes);
+app.use(moviesRoutes);
+app.use(screeningRoutes);
+app.use(bookingRoutes);
+app.use(authenticationRoutes);
+app.use(search);
+app.use(profile);
+
+
+let port = process.env.PORT || 3000;
+let ip = process.env.IP || '127.0.0.1';
+
+app.listen(port, ip, function () {
+	console.log(`Server started at port ${port}`);
+});
